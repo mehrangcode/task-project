c39d329948840aa2c98fdfdab07bc05b688e3982
Mehran <imganjgahi@gmail.com>
2020-07-02 13:18:44 +0430

    no task box

--- src/Components/Tasks/List.jsx
+++ src/Components/Tasks/List.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchList } from '../../Actions/Tasks/Actions'
+import { fetchList, createModal } from '../../Actions/Tasks/Actions'
 import CreateTaskForm from './Create';
 import AddSubTaskForm from '../SubTask/Create';
 import TaskCard from './Card';
 
 import ScrollContainer from 'react-indiana-drag-scroll'
 import Spinner from '../../Utils/Spinner';
+import Access from '../../Utils/Access';
 const TaskList = (props) => {
     const tasks = useSelector(state => state.tasks)
     const dispatch = useDispatch()
@@ -27,9 +28,16 @@ const TaskList = (props) => {
                 <div className="taskList">
                 <Spinner loading={tasks.loading === "fetchTask"} />
 
-                    {tasks.list && tasks.list.map(task => {
+                    {tasks.list && tasks.list.length > 0 ? tasks.list.map(task => {
                         return <TaskCard key={"KEY" + task._id} task={task} />
-                    })}
+                    }) : (
+                        <div className="noTask">
+                            هنوز تسک تعریف نشده است
+                           <Access role="admin">
+                           <p> <button className="btnGold" onClick={() => dispatch(createModal(true))}> ثبت تسک جدید </button> </p>
+                           </Access>
+                        </div>
+                    )}
                 </div>
             </ScrollContainer>
         </React.Fragment>
