4c53e59b8e947d95fa40a6c934a16a7bbbf2250f
Mehran <imganjgahi@gmail.com>
2020-07-02 14:25:10 +0430

    Updates

---
+++ precache-manifest.b04b6f6de41c3682c06d5bea34ed919a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3601672b722121562653973fbdc66b65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "175f151a9fd2e6399d7b",
+    "url": "/static/css/main.a15796f9.chunk.css"
+  },
+  {
+    "revision": "abc54851587f0d6db41d",
+    "url": "/static/js/2.fa9204b1.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/static/js/2.fa9204b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175f151a9fd2e6399d7b",
+    "url": "/static/js/main.90f37640.chunk.js"
+  },
+  {
+    "revision": "227f05271dc90b39cfd8",
+    "url": "/static/js/runtime-main.acea2040.js"
+  },
+  {
+    "revision": "20c3a03c1d0df84fc080df3d855b5703",
+    "url": "/static/media/arrow.20c3a03c.svg"
+  },
+  {
+    "revision": "5c8eb35330b7532dc0b05b5adad6e066",
+    "url": "/static/media/icon.5c8eb353.svg"
+  },
+  {
+    "revision": "fc4f9956de9650b966c9ccb7a4e190ed",
+    "url": "/static/media/loaderIcon.fc4f9956.svg"
+  },
+  {
+    "revision": "fa9054e6a05c286f4e7440a516875a91",
+    "url": "/static/media/loading.fa9054e6.svg"
+  },
+  {
+    "revision": "0e8fbe8e6ff503aaa41ce428170b4563",
+    "url": "/static/media/logout.0e8fbe8e.svg"
+  }
+]);
